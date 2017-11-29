470a0de213a1f3f587fa59373d4bf3b002cb13b3
RKIV <robertkellerIV@gmail.com>
2017-11-29 16:38:52 -0500

    Hi

--- SRCode/src/org/usfirst/frc/team5530/robot/CustomDriveTrain.java
+++ SRCode/src/org/usfirst/frc/team5530/robot/CustomDriveTrain.java
@@ -2,59 +2,60 @@ package org.usfirst.frc.team5530.robot;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+
 
 public class CustomDriveTrain {
 	//Creating Talon Objects
-	SpeedController rDrive1;
-	SpeedController rDrive2;
-	SpeedController lDrive1;
-	SpeedController lDrive2;
+	public static CANTalon rDrive1;
+	public static CANTalon rDrive2;
+	public static CANTalon lDrive1;
+	public static CANTalon lDrive2;
 	
 	//Constructor that assigns talons and controllers to their respective ports
 	CustomDriveTrain(int r1, int r2, int l1, int l2){
 		
-		rDrive1 = new Talon(r1);
-		rDrive2 = new Talon(r2);
-		lDrive1 = new Talon(l1);
-		lDrive2 = new Talon(l2);
+		rDrive1 = new CANTalon(r1);
+		rDrive2 = new CANTalon(r2);
+		lDrive1 = new CANTalon(l1);
+		lDrive2 = new CANTalon(l2);
+		
+		lDrive2.changeControlMode(TalonControlMode.Follower);
+		lDrive2.set(l1);
+		
+		rDrive2.changeControlMode(TalonControlMode.Follower);
+		rDrive2.set(r1);
+		
+		lDrive1.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		lDrive1.configEncoderCodesPerRev(1024);
+		
+		rDrive1.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		rDrive1.configEncoderCodesPerRev(1024);
 	}
-	//A method to limit an input double to the range -1.0 to 1.0
-	public double limit(double prelimNumber){
-		if(prelimNumber >= 1.0){
-			return 1.0;
-					
-		}else if(prelimNumber <= -1.0){
-			
-			return -1.0;
-		}else if(prelimNumber < 1.0 && prelimNumber >-1.0){
-			
-			return prelimNumber;
-		}else{
-			
-			return 0;
-		}
-	}
+	
 	//Calculates right speed based on controller output
 	public double XBControllerR(double lStick, double rTrigger, double lTrigger) {
 		//speed of left side = amount Accelerator is pushed down minus
 		//amount Deccelerator is pushed down - lateral input from left Joystick
-		return limit(rTrigger - lTrigger - lStick);
+		return rTrigger - lTrigger - lStick;
 	}
 	
 	//Calculates left speed based on Controller output
 	public double XBControllerL(double lStick, double rTrigger, double lTrigger){
 		//speed of left side = amount Accelerator is pushed down minus
 		//amount Deccelerator is pushed down + lateral input from left Joystick
-		return limit(rTrigger - lTrigger + lStick);
+		return rTrigger - lTrigger + lStick;
 		
 	}
 	//Sets the speed for both sides using XBController methods
 	public void setSpeeds(double lStick, double rTrigger, double lTrigger){
 		
+		lDrive1.changeControlMode(TalonControlMode.PercentVbus);
+		rDrive1.changeControlMode(TalonControlMode.PercentVbus);
 		rDrive1.set(XBControllerR(lStick, rTrigger, lTrigger));
-		rDrive2.set(XBControllerR(lStick, rTrigger, lTrigger));
 		lDrive1.set(XBControllerL(lStick, rTrigger, lTrigger));
-		lDrive2.set(XBControllerL(lStick, rTrigger, lTrigger));
 		
 		
 	}
